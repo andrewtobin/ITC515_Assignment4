74238142e5985c64b1235ac2afad5575d434dd22
unknown <tobinand@crucible.qafmeats.com.au>
2015-10-16 01:24:57 +1100

    Side effects testing for Bug 01

--- src/Tests/Bug01PayoutOnSingleMatch.cs
+++ src/Tests/Bug01PayoutOnSingleMatch.cs
@@ -33,5 +33,50 @@ namespace Tests
             Assert.Equal(bet, winnings);
             Assert.Equal(funds + bet, player.Balance);
         }
+
+        [Fact]
+        public void SideEffectBetIsDeductedDuringGame()
+        {
+            var die1 = Substitute.For<Dice>();
+            var die2 = Substitute.For<Dice>();
+            var die3 = Substitute.For<Dice>();
+
+            DiceValue pick = DiceValue.ANCHOR;
+            int bet = 10;
+            int winnings = 0;
+            int funds = 100;
+
+            var player = Substitute.For<Player>("Test", funds);
+
+            var game = new Game(die1, die2, die3);
+
+            winnings = game.playRound(player, pick, bet);
+
+            // Make sure at some point that the bet is deducted by the funds.
+            player.Received().takeBet(bet);
+        }
+
+        [Fact]
+        public void SideEffectFundsAreIncrementedByWinningsDuringGame()
+        {
+            var die1 = Substitute.For<Dice>();
+            var die2 = Substitute.For<Dice>();
+            var die3 = Substitute.For<Dice>();
+
+            DiceValue pick = DiceValue.ANCHOR;
+            int bet = 10;
+            int winnings = 0;
+            int funds = 100;
+
+            var player = Substitute.For<Player>("Test", funds);
+
+            var game = new Game(die1, die2, die3);
+
+            winnings = game.playRound(player, pick, bet);
+
+            // Make sure at some point that the winnings are added to the funds during the game.
+            player.Received().receiveWinnings(winnings);
+        }
+
     }
 }
