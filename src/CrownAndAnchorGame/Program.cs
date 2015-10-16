b1a4d6cbe22dcec031d49056664359db0f8b917d
unknown <tobinand@crucible.qafmeats.com.au>
2015-10-17 00:20:31 +1100

    Changes to allow testing of main program.

--- src/CrownAndAnchorGame/Program.cs
+++ src/CrownAndAnchorGame/Program.cs
@@ -52,53 +52,7 @@ namespace CrownAndAnchorGame
 
                 while (true)
                 {
-                    int winCount = 0;
-                    int loseCount = 0;
-                    for (int i = 0; i < 100; i++)
-                    {
-                        p = new Player("Fred", 100);
-                        Console.Write("Start Game {0}: ", i);
-                        Console.WriteLine("{0} starts with balance {1}", p.Name, p.Balance);
-                        int turn = 0;
-                        while (p.balanceExceedsLimitBy(bet) && p.Balance < 200)
-                        {
-                            try
-                            {
-                                winnings = g.playRound(p, pick, bet);
-                                cdv = g.CurrentDiceValues;
-
-                                Console.WriteLine("Rolled {0} {1} {2}", cdv[0], cdv[1], cdv[2]);
-                                if (winnings > 0)
-                                {
-                                    Console.WriteLine("{0} won {1} balance now {2}", p.Name, winnings, p.Balance);
-                                    winCount++;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("{0} lost {1} balance now {2}", p.Name, bet, p.Balance);
-                                    loseCount++;
-                                }
-                            }
-                            catch (ArgumentException e)
-                            {
-                                Console.WriteLine("{0}\n\n", e.Message);
-                            }
-                            pick = Dice.RandomValue;
-                            winnings = 0;
-                            turn++;
-                        } //while
-
-                        Console.Write("{1} turns later.\nEnd Game {0}: ", turn, i);
-                        Console.WriteLine("{0} now has balance {1}\n", p.Name, p.Balance);
-                    } //for
-
-                    Console.WriteLine("Win count = {0}, Lose Count = {1}, {2:0.00}", winCount, loseCount,
-                        (float)winCount / (winCount + loseCount));
-                    totalWins += winCount;
-                    totalLosses += loseCount;
-
-                    string ans = Console.ReadLine();
-                    if (ans.Equals("q")) break;
+                    if (Play100Games(bet, g, ref pick, ref totalWins, ref totalLosses)) break;
                 } //while true
                 Console.WriteLine("Overall win rate = {0}%", (float)(totalWins * 100) / (totalWins + totalLosses));
                 Console.ReadLine();
@@ -107,6 +61,72 @@ namespace CrownAndAnchorGame
             {
                 Log.Fatal(ex, "Quit Application by force\n\n\n\n");
             }
+        }
+
+        internal static bool Play100Games(int bet, Game g, ref DiceValue pick, ref int totalWins, ref int totalLosses)
+        {
+            int winCount = 0;
+            int loseCount = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                winCount = PlayGame(bet, g, ref pick, i, winCount, ref loseCount);
+            } //for
+
+            Console.WriteLine("Win count = {0}, Lose Count = {1}, {2:0.00}", winCount, loseCount,
+                (float) winCount/(winCount + loseCount));
+            totalWins += winCount;
+            totalLosses += loseCount;
+
+            string ans = Console.ReadLine();
+            if (ans.Equals("q")) return true;
+            return false;
+        }
+
+        private static int PlayGame(int bet, Game g, ref DiceValue pick, int i, int winCount, ref int loseCount)
+        {
+            Player p;
+            p = new Player("Fred", 100);
+            Console.Write("Start Game {0}: ", i);
+            Console.WriteLine("{0} starts with balance {1}", p.Name, p.Balance);
+            int turn = 0;
+            while (p.balanceExceedsLimitBy(bet) && p.Balance < 200)
+            {
+                int winnings;
+                try
+                {
+                    winCount = PlayRound(bet, g, pick, winCount, ref loseCount, p);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine("{0}\n\n", e.Message);
+                }
+                pick = Dice.RandomValue;
+                winnings = 0;
+                turn++;
+            } //while
+
+            Console.Write("{1} turns later.\nEnd Game {0}: ", turn, i);
+            Console.WriteLine("{0} now has balance {1}\n", p.Name, p.Balance);
+            return winCount;
+        }
+
+        private static int PlayRound(int bet, Game g, DiceValue pick, int winCount, ref int loseCount, Player p)
+        {
+            var winnings = g.playRound(p, pick, bet);
+            var cdv = g.CurrentDiceValues;
+
+            Console.WriteLine("Rolled {0} {1} {2}", cdv[0], cdv[1], cdv[2]);
+            if (winnings > 0)
+            {
+                Console.WriteLine("{0} won {1} balance now {2}", p.Name, winnings, p.Balance);
+                winCount++;
+            }
+            else
+            {
+                Console.WriteLine("{0} lost {1} balance now {2}", p.Name, bet, p.Balance);
+                loseCount++;
+            }
+            return winCount;
         }
     }
 }
